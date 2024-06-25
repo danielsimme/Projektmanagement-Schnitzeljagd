2f97f022b4661f7e9b89744912b5cbede3ab3a0c
Matteo Stadelmann <matteo.stadelmann@hispeed.ch>
2024-06-25 10:41:38 +0200

    Initial | Started Styles

--- index.php
+++ index.php
@@ -2,65 +2,27 @@
 <html lang="de">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Meine Webseite</title>
-    <!-- Font import -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-
-    <!-- External CSS -->
-    <link rel="stylesheet" href="styles.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Question Form</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <header>
-        <div class="logo">Schnitzeljagt</div>
-        <div class="nav-container">
-            <nav>
-                <ul>
-                    <li><a href="index.php">Home</a></li>
-                    <li><a href="form-page-1.php">Contact</a></li>
-                </ul>
-            </nav>
+    <div class="container">
+        <img src="img/IMG_3111.jpg" alt="Image">
+        <div class="question">
+            Wie viele Pilze stehen auf dem Platz vor dem Schulhaus?
         </div>
-    </header>
-    <div class="title-section">
-        <h1>ALL OUR <span>PROJECTS</span></h1>
+        <input type="text" name="answer" placeholder="Your answer">
+        <div class="buttons">
+            <button class="back">&lt; Back</button>
+            <button class="next">Next &gt;</button>
+        </div>
+        <div class="footer">
+            <p>QUESTION 1 OF 4</p>
+        </div>
     </div>
-
-
-    <footer>
-        <div class="social-icons">
-            <a href="https://x.com" target="_blank">
-                <img src="Icons/Twitter.svg" alt="Twitter">
-            </a>
-            <a href="https://instagram.com" target="_blank">
-                <img src="Icons/Instagram.svg" alt="Instagram">
-            </a>
-            <a href="https://youtube.com" target="_blank">
-                <img src="Icons/Youtube.svg" alt="YouTube">
-            </a>
-            <a href="https://linkedin.com" target="_blank">
-                <img src="Icons/LinkedIn.svg" alt="LinkedIn">
-            </a>
-            <a href="https://facebook.com" target="_blank">
-                <img src="Icons/Facebook.svg" alt="Facebook">
-            </a>
-        </div>
-        <div class="footer-logo">dandrew</div>
-    </footer>
-
 </body>
 
 </html>
